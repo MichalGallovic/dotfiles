b7d3c5662259d3746ce1a3a791ae0d1322c3fd7c
MichalGallovic <gallovicm@gmail.com>
2020-04-12 15:05:58 +0200

    Add script to backup apps, ADd script to restore apps, Update brewfile

---
+++ backup-apps.sh
@@ -0,0 +1,5 @@
+# Cyberduck bookmarks
+cp -R ~/Library/Group\ Containers/G69SCX94XU.duck/Library/Application\ Support/duck/Bookmarks ~/Dropbox/Mackup/Applications/Cyberduck
+
+# Sublime text 3
+cp -R ~/Library/Application\ Support/Sublime\ Text\ 3 ~/Dropbox/Mackup/Applications/Sublime\ Text\ 3
---
+++ restore-apps.sh
@@ -0,0 +1,5 @@
+# Cyberduck
+cp -R ~/Dropbox/Mackup/Applications/Cyberduck/Bookmarks ~/Library/Group\ Containers/G69SCX94XU.duck/Library/Application\ Support/duck/
+
+# Sublime text 3
+cp -R ~/Dropbox/Mackup/Applications/Sublime\ Text\ 3 ~/Library/Application\ Support/Sublime\ Text\ 3
