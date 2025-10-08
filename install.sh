def8aaa7a15abc0fd99b474fd671981041864a29
MichalGallovic <gallovicm@gmail.com>
2025-10-08 18:05:14 +0200

    wip

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+cp $HOME/.dotfiles/.to-copy-zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
